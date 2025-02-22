1ba9fbef9c27fc7383fc57de6a7b6a84278014cf
kannan1674 <168915354+kannan1674@users.noreply.github.com>
2025-02-22 14:31:52 +0530

    Update server.js

--- server/server.js
+++ server/server.js
@@ -4,8 +4,8 @@ const DB = require('./config/database');
 
 // Initialize the database connection
 DB();
+const PORT = process.env.PORT || 8000;
 
-// Start the server and listen on the specified port and host (0.0.0.0 for external access)
-app.listen(process.env.PORT, '0.0.0.0', () => {
-  console.log(`Server Successfully Run on Port ${process.env.PORT} in ${process.env.NODE_ENV} mode`);
+app.listen(PORT, '0.0.0.0', () => {
+  console.log(`Server Successfully Run on Port ${PORT} in ${process.env.NODE_ENV || 'development'} mode`);
 });
